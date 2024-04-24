97842baf882d3e6a15d93fc6f85adcf3b0cd023a
Vitam <vitamin.lapickis@gmail.com>
2024-04-24 13:36:58 +0300

    gggg

--- src/main/java/Homework/Alphabet.java
+++ src/main/java/Homework/Alphabet.java
@@ -8,50 +8,33 @@ public class Alphabet {
     }
 
     public static String sortString(String s) {
-        // Базовый случай: если длина строки меньше или равна 1, вернуть строку
         if (s.length() <= 1) {
             return s;
         }
-
-        // Находим середину строки
         int mid = s.length() / 2;
-
-        // Разделяем строку на две части
         String left = s.substring(0, mid);
         String right = s.substring(mid);
-
-        // Рекурсивно сортируем каждую половину
         String sortedLeft = sortString(left);
         String sortedRight = sortString(right);
-
-        // Объединяем и сортируем две отсортированные части
         return merge(sortedLeft, sortedRight);
     }
 
-    // Метод для слияния и сортировки двух строк
     public static String merge(String left, String right) {
         StringBuilder result = new StringBuilder();
         int leftPtr = 0, rightPtr = 0;
-
-        // Пока указатели не достигнут конца хотя бы одной из строк
         while (leftPtr < left.length() && rightPtr < right.length()) {
-            // Сравниваем символы в двух строках и добавляем меньший в результирующую строку
             if (left.charAt(leftPtr) < right.charAt(rightPtr)) {
                 result.append(left.charAt(leftPtr++));
             } else {
                 result.append(right.charAt(rightPtr++));
             }
         }
-
-        // Добавляем оставшиеся символы из левой и правой строки, если они есть
         while (leftPtr < left.length()) {
             result.append(left.charAt(leftPtr++));
         }
-
         while (rightPtr < right.length()) {
             result.append(right.charAt(rightPtr++));
         }
-
         return result.toString();
     }
 }
---
+++ src/main/java/Homework/Key.java
@@ -0,0 +1,29 @@
+package Homework;
+import java.security.*;
+public class Key {
+
+    public static void main(String[] args) {
+
+        try {
+            // Генерация ключей
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+            // Получение публичного и приватного ключей
+            PublicKey publicKey = keyPair.getPublic();
+            PrivateKey privateKey = keyPair.getPrivate();
+/
+            // Вывод публичного ключа
+            System.out.println("Публичный ключ RSA:");
+            System.out.println(publicKey);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Ошибка: Невозможно создать ключ RSA.");
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
